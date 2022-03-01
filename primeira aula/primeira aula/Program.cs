20653f675af4de7a2016d213e3c6a9c2b313c632
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2022-03-01 16:05:47 -0300

    Aula 1

---
+++ primeira aula/primeira aula/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem vindos!!!");
+            Console.WriteLine("Enviar o comprovante");
+            MeuPrograma();
+        }
+        //criar uma função
+        static void MeuPrograma() 
+        {
+            Console.WriteLine("Final da aula");
+            Console.WriteLine("Até segunda!");
+        }
+    }
+}
